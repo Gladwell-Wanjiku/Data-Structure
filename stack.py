cee72a02ec8493e29c5bff7a21612cb88d123ddf
Gladwell-Wanjiku <wanjeriwanjikugladwell@gmail.com>
2024-03-31 11:48:23 +0300

    add file

---
+++ stack.py
@@ -0,0 +1,17 @@
+myStack= []
+myStack.append("blue")
+myStack.append("purple")
+myStack.append("maroon")
+myStack.append("black")
+myStack.append("white")
+print(myStack)
+#removing an item
+print(myStack.pop())
+
+#accessing items
+print(myStack[1])
+
+#adding items
+myStack = ["blue", "purple", "maroon", "black", "white"]
+myStack.append("violet")
+print(myStack)
